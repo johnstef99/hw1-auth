ee291f0da803fb494a0064616fc3b6700a4ca5be
johnstef99 <john@johnstef.com>
2023-01-29 00:07:46 +0200

    change BaudController50MHz to enable output for one cycle

--- src/BaudController50MHz.v
+++ src/BaudController50MHz.v
@@ -10,14 +10,14 @@ module BaudController50MHz (
   reg [13:0] counter;
 
   initial begin
-    limit[3'b000] = 14'd10417; // 300    bits/sec
-    limit[3'b001] = 14'd2604;  // 1200   bits/sec
-    limit[3'b010] = 14'd651;   // 4800   bits/sec
-    limit[3'b011] = 14'd326;   // 9600   bits/sec
-    limit[3'b100] = 14'd163;   // 19200  bits/sec
-    limit[3'b101] = 14'd81;    // 38400  bits/sec
-    limit[3'b110] = 14'd54;    // 57600  bits/sec
-    limit[3'b111] = 14'd27;    // 115200 bits/sec
+    limit[3'b000] = 14'd10417;  // 300    bits/sec
+    limit[3'b001] = 14'd2604;  //  1200   bits/sec
+    limit[3'b010] = 14'd651;  //   4800   bits/sec
+    limit[3'b011] = 14'd326;  //   9600   bits/sec
+    limit[3'b100] = 14'd163;  //   19200  bits/sec
+    limit[3'b101] = 14'd81;  //    38400  bits/sec
+    limit[3'b110] = 14'd54;  //    57600  bits/sec
+    limit[3'b111] = 14'd27;  //    115200 bits/sec
   end
 
   always @(posedge clk or posedge reset) begin
@@ -25,11 +25,11 @@ module BaudController50MHz (
       counter  <= 0;
       baud_out <= 0;
     end else begin
-      if (counter == limit[baud_sel]) begin
+      if (counter == limit[baud_sel] - 1) begin
         counter  <= 0;
         baud_out <= 1;
       end else begin
-        counter  <= counter + 1;
+        counter <= counter + 1;
         baud_out <= 0;
       end
     end
