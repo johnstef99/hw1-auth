ca4e3522b942d3d15358cc03c632ca3cd7a89fd8
johnstef99 <john@johnstef.com>
2023-01-30 15:32:08 +0200

    update testbenches

--- src/BaudController50MHz_tb.v
+++ src/BaudController50MHz_tb.v
@@ -5,11 +5,13 @@ module BaudController50MHz_tb;
 
   reg clk_tb, reset_tb;
   reg [2:0] baud_sel_tb;
+  wire baud_out_tb;
 
   BaudController50MHz baud_contr (
       .clk(clk_tb),
       .reset(reset_tb),
-      .baud_sel(baud_sel_tb)
+      .baud_sel(baud_sel_tb),
+      .baud_out(baud_out_tb)
   );
 
   always #10 clk_tb = ~clk_tb;
@@ -25,7 +27,14 @@ module BaudController50MHz_tb;
     #1 reset_tb = 1;
     #1 reset_tb = 0;
 
-    #2000000 $finish;
+    repeat(10) @(negedge baud_out_tb);
+
+    baud_sel_tb = 3'b011;
+
+    repeat(10) @(negedge baud_out_tb);
+
+
+    $finish;
   end
 
 endmodule
